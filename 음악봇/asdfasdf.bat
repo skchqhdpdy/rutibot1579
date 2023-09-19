98c6631adc7d8c44523cf43619e24ae9ee2c0a44
전강현 <aniren@debian.moe>
2023-09-19 21:17:13 +0900

    음악봇

---
+++ 음악봇/asdfasdf.bat
@@ -0,0 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAhfTwyTNG+qAqwgzO3o5P6IsnFVddUMMc9WL0gF3hGZP4loj0JthzQLz4uH3v/VnMWXSgYnqmFQsiSnONOgPvodwbZ4psDk/Fl9hGyehSg0AA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAhfTwyTNG+qAqwgzO3o5P6IsnFVddUMMc9WL0gF3hGZP4loj0JthzQLz4uH3v/VnMWXSgYnqmFQsiSnONOgPvodwbZ4psfN9+mBYugEaxcodNJ6jpFN1ji7nA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cd C:\Users\skchqhdpdy\Desktop\(AWS) ?œëž‘êµ¬ìš´ 1.17 ë²„í‚·\Discord Bot\?œëž‘ ?Œì•…ë´?start.exe
+pause
